c4f6c58dcf3d4b3f7511e8a17dbc7c074c6dc848
Kacper Soczko <kacper.soczko@gmail.com>
2025-12-03 22:52:07 +0100

    Move debug mode behind environment variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-08gw%1k=n3(!!%xe7do!o(!92z4!(4z#8y%$6%%bjxdasipq%)'
+DEBUG = os.environ.get("PRODUCTION") is None
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if DEBUG:
+    SECRET_KEY = 'django-insecure-08gw%1k=n3(!!%xe7do!o(!92z4!(4z#8y%$6%%bjxdasipq%)'
+else:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 ALLOWED_HOSTS = []
 
