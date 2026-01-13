5ec34c7d92ad4b73919401d41c38a9f7bb602b03
Kacper Soczko <kacper.soczko@gmail.com>
2026-01-13 21:18:58 +0100

    Add comments capabilities

--- app/comments/models.py
+++ app/comments/models.py
@@ -1,8 +1,25 @@
+from django.contrib.auth.forms import User
 from django.db import models
+import rules
+from rules.contrib.models import RulesModel
 
-from ..tables.models import Table
 
-class Comment(models.Model):
+from tables.models import Table
+
+@rules.predicate
+def is_comment_creator(user, comment):
+    return user == comment.creator
+
+class Comment(RulesModel):
+    class Meta:
+        rules_permissions = {
+            "change": is_comment_creator,
+            "delete": is_comment_creator,
+        }
+
     comment = models.TextField()
-    creator = models.ForeignKey(Table, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE, related_name="comments")
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now=True)
+
+
--- app/comments/urls.py
+++ app/comments/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from app.comments.views import CommentCreateView, CommentDeleteView, CommentUpdateView, CommentsListView
+from .views import CommentCreateView, CommentDeleteView, CommentUpdateView
 
 
 urlpatterns = [
-    path('', CommentsListView.as_view(), name="comments_list_view"),
     path('create/', CommentCreateView.as_view(), name="comment_create_view"),
-    path('<int:pk>/delete/', CommentDeleteView.as_view(), name="comment_delete_view"),
-    path('<int:pk>/update/', CommentUpdateView.as_view(), name="comment_update_view"),
+    path('<int:comment_pk>/delete/', CommentDeleteView.as_view(), name="comment_delete_view"),
+    path('<int:comment_pk>/update/', CommentUpdateView.as_view(), name="comment_update_view"),
 ]
